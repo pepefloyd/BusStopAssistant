f366165fa94d9fa7c792447ea678e91631c24262
Jose Garcia Ponce <pepefloyd@gmail.com>
2020-04-15 01:33:48 +0100

    added better response handling and support for SSML

--- DublinOnTime/main.py
+++ DublinOnTime/main.py
@@ -9,6 +9,7 @@ Ireland, March 2020.
 import json
 import logging
 import random
+import re
 from enum import Enum, IntEnum
 
 from falcon import API, MEDIA_JSON, HTTP_200
@@ -50,26 +51,30 @@ class BusStopRequest():
             dlg_flow_req = DialogflowRequest(json.dumps(json_request))
             stop_param = '' if 'stop' not in dlg_flow_req.get_parameters() \
                 else dlg_flow_req.get_parameters().get('stop')
-            if dlg_flow_req.get_action() == 'call_busstop_api' and stop_param:
+            match_numbers = re.findall('\d+', stop_param)
+            if dlg_flow_req.get_action() == 'call_busstop_api' and stop_param and match_numbers:
                 LOGGER.info('stop parameter is {}'.format(stop_param))
                 if ' to ' in stop_param:
                     # This converts '70 to 94' to '7294'
                     index = stop_param.index(' to ')
                     stop_param = stop_param[:index - 1] + '2' + stop_param[index + 4:]
-                stop_param = stop_param.replace(' ', '')
+                else:
+                    stop_param = match_numbers[0]  # This will grab the digits in the string
+                stop_param = stop_param.replace(' ', '')  # ensure we don't have spaces
                 bus_stop = int(stop_param.split('.', 1)[0])
                 LOGGER.info("Resolved bus stop: {}".format(bus_stop))
                 query_response = self.query_bus_stop(bus_stop)
                 bus_times_response_state = self.deserialize_response(query_response)
-                good_response = BusStopResponse(bus_times_response_state).response_message
-                final_response = good_response + BusStopResponse.get_goodbye_message()
-                LOGGER.info('setting response to {}'.format(final_response))
-                resp.body = self.create_dialogflow_response(final_response)
+                response = BusStopResponse(bus_times_response_state)
+                response_message = response.response_message
+                final_response = response_message + BusStopResponse.get_goodbye_message()
+                LOGGER.info('Setting response to {}'.format(final_response))
+                resp.body = response.create_google_response(final_response)
                 resp.content_type = MEDIA_JSON
                 resp.status = HTTP_200
             elif dlg_flow_req.get_action() == 'call_busstop_api':
-                LOGGER.info('bus stop request did no contain stop parameter')
-                resp.body = self.create_dialogflow_response(
+                LOGGER.info('bus stop request did no contain valid stop parameter')
+                resp.body = self.create_google_response(
                     BusStopResponse.get_greeting_with_question(), True)
                 resp.content_type = MEDIA_JSON
                 resp.status = HTTP_200
@@ -78,20 +83,14 @@ class BusStopRequest():
         except Exception as error:
             LOGGER.error('There was an error processing this request')
             LOGGER.error(str(error))
-            resp.body = self.create_dialogflow_response(BusStopResponse.get_error_message())
+            resp.body = self.create_google_response(BusStopResponse.get_error_message())
             resp.content_type = MEDIA_JSON
             resp.status = HTTP_200
-
-    def create_dialogflow_response(self, response, expect_user_response=False):
-        """ Build the dialogflow response in the correct format"""
-        dialogflow_response = DialogflowResponse()
-        dialogflow_response.expect_user_response = expect_user_response
-        dialogflow_response.add(SimpleResponse(response, response))
-        return dialogflow_response.get_final_response()
 
     def query_bus_stop(self, stop_number):
         """ Build full query string to send to RTPI site"""
         return self.send_request(self.get_rtpi_site() + '?stopRef=' + str(stop_number))
+
 
     @staticmethod
     def get_rtpi_site():
@@ -137,7 +136,7 @@ class BusStopResponse():
 
     many_buses_message = [' These buses are coming soon: ', ' The following buses are coming:  ',
                           ' These are the buses coming to your bus stop: ']
-    goodbye_message = [' ', '  Goodbye!', ' Have a nice day!', ' Have a great day!', '  Adios!']
+    goodbye_message = [' Have a safe trip!', ' Goodbye!', ' Have a nice day!', ' Have a great day!', '  Adios!']
 
     def __init__(self, bus_response):
 
@@ -147,10 +146,27 @@ class BusStopResponse():
         self.set_availability()
         self.set_message()
 
+    @staticmethod
+    def make_ssml_message(message):
+        return '<speak>' + message + '</speak>'
+
+    def create_google_response(self, message, expect_user_response=False):
+        """ Build the dialogflow response in the correct format"""
+
+        google_response = DialogflowResponse()
+        google_response.expect_user_response = expect_user_response
+        google_response.add(SimpleResponse(self.get_text_only_message(message),
+                                               self.make_ssml_message(message)))
+        return google_response.get_final_response()
+
     @classmethod
     def get_greeting_with_question(cls):
         """ To be sent as an initial greeting when app is launch with parameters"""
         return random.choice(cls.greetings)
+
+    @staticmethod
+    def get_text_only_message(message):
+        return re.sub('<[^>]+>', '', message)
 
     @classmethod
     def get_error_message(cls):
@@ -160,17 +176,31 @@ class BusStopResponse():
     @classmethod
     def get_single_bus_message_initial_greeting(cls):
         """ initial greeting to a single bus prior to replying"""
-        return random.choice(cls.single_bus_message)
+        return cls.make_paragraph(random.choice(cls.single_bus_message))
 
     @classmethod
     def get_goodbye_message(cls):
         """ message to be appended to a final positive response"""
-        return random.choice(cls.goodbye_message)
+        return cls.add_speech_pause(random.choice(cls.goodbye_message), 'before', '2s' )
 
     @classmethod
     def get_many_buses_initial_greeting(cls):
         """ initial greeting for multiple buses prior to replying"""
-        return random.choice(cls.many_buses_message)
+        return cls.make_paragraph(random.choice(cls.many_buses_message))
+
+    @staticmethod
+    def make_paragraph(message):
+        return '<p><s>' + message + '</s></p>'
+
+    @staticmethod
+    def add_speech_pause(message, when, time):
+        # use SSML syntax to define time, i.e 1s or 500ms
+        # when: before or after
+        if when == 'before':
+            return '<break time=\"' + time + '\"/>' + message
+        elif when == 'after':
+            return message + '<break time=\"' + time + '\"/>'
+        return message
 
     def set_message(self):
         """Sets the correct message"""
@@ -183,6 +213,7 @@ class BusStopResponse():
             self.response_message = 'I could not find any buses arriving at this bus stop.'
 
         if self.availability == Availability.MANY_BUSES or self.availability == Availability.ONE_BUS:
+            LOGGER.info('Buses are available. Building message')
             bus_details_message = ', '.join(self.get_incoming_buses_detailed_message(self.bus_response))
             self.response_message = self.response_message + bus_details_message
 
@@ -220,12 +251,13 @@ class BusStopResponse():
             """
             Prepare a user-friendly message
             """
-            message = service_time[0] + ' '
+            message = str(service_time[0]) + ' '
             if is_due(service_time[1]):
                 message += ' is due'
             elif is_time(service_time[1]):
                 # 22:05 changes to '22 05'
-                message += ' is coming at ' + service_time[1].replace(':', ' ')
+                message += ' is coming at <say-as interpret-as="time" format="24"> ' +\
+                           service_time[1] + '</say-as>'
             else:
                 message += service_time[1].replace('Mins', 'minutes')
             return message
